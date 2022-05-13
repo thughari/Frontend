325f5c49e1bfa9cb02f6168e2688d43cf4ee97a1
Hari <haribabutatikonda3@gmail.com>
2022-05-13 20:10:53 +0530

    13 may

---
+++ Eclips-workspace/Java8PracticalQuestions/src/java8assignment/CustomerTest.java
@@ -0,0 +1,11 @@
+package java8assignment;
+
+public class CustomerTest {
+
+	public static void main(String[] args) {
+		var obj= new Customer(1001, "haribabutatikonda3#gmail.com", "password", "Hari", "babu", "chirala", "male", "9032805118");
+		System.out.println(obj.toString());
+
+	}
+
+}
