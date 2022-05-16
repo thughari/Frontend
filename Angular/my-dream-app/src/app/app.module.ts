448c4e01fe48b0666504bf5fae876385396c129d
Hari <haribabutatikonda3@gmail.com>
2022-05-16 19:02:37 +0530

    may 16

--- Angular/my-dream-app/src/app/app.module.ts
+++ Angular/my-dream-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
